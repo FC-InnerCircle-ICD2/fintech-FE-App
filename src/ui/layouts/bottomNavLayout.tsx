143dc81313490ba4f4cc8bc4055b8b049e5423c8
jinsewoon-dev <jinsewoon.dev@gmail.com>
2025-02-18 01:43:53 +0900

    feat: 헤더에 로그아웃 기능을 담은 버튼 추가

--- src/ui/layouts/bottomNavLayout.tsx
+++ src/ui/layouts/bottomNavLayout.tsx
@@ -2,17 +2,23 @@ import { Outlet } from 'react-router-dom';
 import { cn } from '@lib/shadcn/lib/utils';
 import { theme } from '@styles/theme';
 import { BottomNavigation } from '@ui/templates/navigation/bottomNav';
+import Button from '@ui/components/button/Button';
+import { useAuth } from '@hooks/useAuth';
 
 export const BottomNavLayout = () => {
+  const { logout } = useAuth();
   return (
     <div className='h-[100dvh]'>
       <header
         className={cn(
-          `sticky bg-white  flex items-center top-0 h-14 w-responsive_container mx-auto border-b`,
+          `sticky bg-white justify-between flex items-center top-0 h-14 w-responsive_container mx-auto border-b`,
           theme.safe_area_inline_padding,
         )}
       >
         <img src='/logo.png' width={24} />
+        <Button variant={'outline_ghost'} width={'fit'} onClick={logout}>
+          logout
+        </Button>
       </header>
       <main className={cn('w-responsive_container mx-auto pb-[4rem] bg-white')}>
         <Outlet />
