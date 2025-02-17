5b9177e663cbf68b0445e34683bd64148a08fddf
annakim <kan12888@gmail.com>
2025-02-18 01:18:23 +0900

    feat: add dummy card list

--- src/mocks/config/dummy.ts
+++ src/mocks/config/dummy.ts
@@ -8,3 +8,74 @@ export const DUMMY_API_CONFIG = {
     IAT: 171232323232,
   },
 } as const;
+
+/**
+ * 카드 목록
+ * TODO: owner 추가요청
+ *
+ *
+ * @description 카드 목록을 반환합니다.
+ * @returns 카드 목록
+ */
+export const DUMMY_CARD_LIST = [
+  {
+    id: '1',
+    isRepresentative: true,
+    cardCompany: 'VISA',
+    cardNumber: '**** **** **** 1234',
+    owner: '김철수',
+    expirationPriod: '12/26',
+    cvc: '123',
+    type: 'credit',
+    createdAt: '2024-02-11T10:00:00Z',
+    updatedAt: '2024-02-11T12:00:00Z',
+  },
+  {
+    id: '2',
+    isRepresentative: false,
+    cardCompany: 'VISA',
+    cardNumber: '**** **** **** 5678',
+    owner: '김철수',
+    expirationPriod: '07/25',
+    cvc: '123',
+    type: 'credit',
+    createdAt: '2024-01-20T14:30:00Z',
+    updatedAt: '2024-02-05T08:45:00Z',
+  },
+  {
+    id: '3',
+    isRepresentative: false,
+    cardCompany: 'VISA',
+    cardNumber: '**** **** **** 9012',
+    owner: '이영희',
+    expirationPriod: '09/27',
+    cvc: '123',
+    type: 'credit',
+    createdAt: '2023-12-15T09:15:00Z',
+    updatedAt: '2024-01-10T09:15:00Z',
+  },
+  {
+    id: '4',
+    isRepresentative: true,
+    cardCompany: 'VISA',
+    cardNumber: '**** **** **** 3456',
+    owner: '박민수',
+    expirationPriod: '03/28',
+    cvc: '123',
+    type: 'credit',
+    createdAt: '2023-11-08T16:20:00Z',
+    updatedAt: '2024-02-10T10:00:00Z',
+  },
+  {
+    id: '5',
+    isRepresentative: false,
+    cardCompany: 'VISA',
+    cardNumber: '**** **** **** 7890',
+    owner: '최지훈',
+    expirationPriod: '06/26',
+    cvc: '123',
+    type: 'credit',
+    createdAt: '2023-10-05T11:45:00Z',
+    updatedAt: '2024-01-25T14:30:00Z',
+  },
+];
