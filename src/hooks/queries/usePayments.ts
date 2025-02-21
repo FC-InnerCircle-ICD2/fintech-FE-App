24f8ad67fd58c717ff237ac446cf9fb1483bd074
jinsewoon-dev <jinsewoon.dev@gmail.com>
2025-02-21 17:16:19 +0900

    refactor: useTransactionList ,useTransactionDetail의 select 수정

--- src/hooks/queries/usePayments.ts
+++ src/hooks/queries/usePayments.ts
@@ -1,12 +1,16 @@
 import { paymentService } from '@api/services/payment';
-import { useQuery, useMutation } from '@tanstack/react-query';
+import { useQuery, useMutation, useSuspenseQuery } from '@tanstack/react-query';
 import { QUERY_KEY } from '@constants/apiEndpoints';
 
 export const useOrderInfo = (orderToken: string) => {
   return useQuery({
     queryKey: [QUERY_KEY.PAYMENT.ORDER_INFO, orderToken],
     queryFn: () => paymentService.getOrderInfo(orderToken),
-    select: (response) => response.data,
+    select: (response) => {
+      if (response.ok) {
+        return response.data;
+      }
+    },
   });
 };
 
@@ -22,11 +26,17 @@ export const useCancelPayment = () => {
   });
 };
 
-export const useTransactionList = () => {
-  return useQuery({
-    queryKey: [QUERY_KEY.MANAGEMENT.HISTORY],
-    queryFn: () => paymentService.getTransactionList(),
-    select: (response) => response.data,
+export const useTransactionList = (page: number, limit: number) => {
+  return useSuspenseQuery({
+    queryKey: [QUERY_KEY.MANAGEMENT.HISTORY, page, limit], // ✅ queryKey에 page, limit 추가
+    queryFn: () => paymentService.getTransactionList({ page, limit }), // ✅ page, limit을 인자로 전달
+    select: (response) => {
+      if (response.ok) {
+        return response.data.payments;
+      }
+      throw new Error(response.error.message); // ✅ 에러 핸들링 추가
+    },
+    // placeholderData: (previousData) => previousData ?? undefined, // ✅ 페이지네이션 시 이전 데이터 유지
   });
 };
 
@@ -34,6 +44,10 @@ export const useTransactionDetail = (id: string) => {
   return useQuery({
     queryKey: [QUERY_KEY.MANAGEMENT.HISTORY, id],
     queryFn: () => paymentService.getTransactionDetail(id),
-    select: (response) => response.data,
+    select: (response) => {
+      if (response.ok) {
+        return response.data;
+      }
+    },
   });
 };
