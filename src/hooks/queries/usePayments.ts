901524535b6c5459d2ddd916f989cceda63b2a13
jinsewoon-dev <jinsewoon.dev@gmail.com>
2025-02-22 06:03:05 +0900

    chore: build 에러 수정

--- src/hooks/queries/usePayments.ts
+++ src/hooks/queries/usePayments.ts
@@ -2,23 +2,23 @@ import { paymentService } from '@api/services/payment';
 import { useQuery, useMutation, useSuspenseQuery } from '@tanstack/react-query';
 import { QUERY_KEY } from '@constants/apiEndpoints';
 
-export const useOrderInfo = (orderToken: string) => {
-  return useQuery({
-    queryKey: [QUERY_KEY.PAYMENT.ORDER_INFO, orderToken],
-    queryFn: () => paymentService.getOrderInfo(orderToken),
-    select: (response) => {
-      if (response.ok) {
-        return response.data;
-      }
-    },
-  });
-};
+// export const useOrderInfo = (orderToken: string) => {
+//   return useQuery({
+//     queryKey: [QUERY_KEY.PAYMENT.ORDER_INFO, orderToken],
+//     queryFn: () => paymentService.getOrderInfo(orderToken),
+//     select: (response) => {
+//       if (response.ok) {
+//         return response.data;
+//       }
+//     },
+//   });
+// };
 
-export const useRequestPayment = () => {
-  return useMutation({
-    mutationFn: (orderId: string) => paymentService.requestPayment(orderId),
-  });
-};
+// export const useRequestPayment = () => {
+//   return useMutation({
+//     mutationFn: (orderId: string) => paymentService.requestPayment(orderId),
+//   });
+// };
 
 export const useCancelPayment = () => {
   return useMutation({
