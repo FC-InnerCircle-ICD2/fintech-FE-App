a29d3d88ba95ee2a514de09c062ff3a5b1bc247a
jinsewoon-dev <jinsewoon.dev@gmail.com>
2025-02-22 09:35:19 +0900

    refactor: 결제내역 파라미터 추가

--- src/hooks/queries/usePayments.ts
+++ src/hooks/queries/usePayments.ts
@@ -1,6 +1,7 @@
 import { paymentService } from '@api/services/payment';
 import { useQuery, useMutation, useSuspenseQuery } from '@tanstack/react-query';
 import { QUERY_KEY } from '@constants/apiEndpoints';
+import type { TransactionStatus } from '@type/payment';
 
 // export const useOrderInfo = (orderToken: string) => {
 //   return useQuery({
@@ -26,10 +27,38 @@ export const useCancelPayment = () => {
   });
 };
 
-export const useTransactionList = (page: number, limit: number) => {
+export type TransactionListFilter = {
+  startDate: string;
+  endDate: string;
+  status: TransactionStatus | 'null';
+  page: number;
+  limit: number;
+};
+
+export const useTransactionList = ({
+  startDate,
+  endDate,
+  status,
+  page,
+  limit,
+}: TransactionListFilter) => {
   return useSuspenseQuery({
-    queryKey: [QUERY_KEY.MANAGEMENT.HISTORY, page, limit],
-    queryFn: () => paymentService.getTransactionList({ page, limit }),
+    queryKey: [
+      QUERY_KEY.MANAGEMENT.HISTORY,
+      startDate,
+      endDate,
+      status,
+      page,
+      limit,
+    ],
+    queryFn: () =>
+      paymentService.getTransactionList({
+        startDate,
+        endDate,
+        status,
+        page,
+        limit,
+      }),
     select: (response) => {
       if (response.ok) {
         return response.data.payments;
